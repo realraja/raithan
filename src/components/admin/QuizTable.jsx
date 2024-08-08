d4b5756a3bcf3703247ee63381c315d64c02998c
realraja <146420691+realraja@users.noreply.github.com>
2024-08-08 20:43:01 +0530

    error when not published

--- src/components/admin/QuizTable.jsx
+++ src/components/admin/QuizTable.jsx
@@ -18,22 +18,29 @@ import moment from "moment";
 import { useRouter } from "next/navigation";
 import UpdateQuiz from "../Dialogs/EditQuiz";
 
-
 const UserTable = ({ data }) => {
-  const linkWeb = window.location.href.split('raithan-add')[0] + 'start-quiz/';
-    const router = useRouter();
+  const linkWeb = window.location.href.split("raithan-add")[0] + "start-quiz/";
+  const router = useRouter();
   const [searchInput, setSearchInput] = useState("");
   const [filteredData, setFilteredData] = useState(data);
 
   const [editQuizDialog, setEditQuizDialog] = useState(false);
   const [editQuiz, setEditQuiz] = useState(null);
-  
-  const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0, studentId: null });
+
+  const [contextMenu, setContextMenu] = useState({
+    visible: false,
+    x: 0,
+    y: 0,
+    studentId: null,
+  });
   const contextMenuRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (contextMenuRef.current && !contextMenuRef.current.contains(event.target)) {
+      if (
+        contextMenuRef.current &&
+        !contextMenuRef.current.contains(event.target)
+      ) {
         setContextMenu({ ...contextMenu, visible: false });
       }
     };
@@ -53,14 +60,11 @@ const UserTable = ({ data }) => {
   const handleSearch = (e) => {
     const value = e.target.value.toLowerCase();
     setSearchInput(value);
-    const filtered = data.filter(
-      (quiz) =>
-        quiz.name.toLowerCase().includes(value) 
+    const filtered = data.filter((quiz) =>
+      quiz.name.toLowerCase().includes(value)
     );
     setFilteredData(filtered);
   };
-
-
 
   const handlePublishQuiz = adminTryCatch(async (id) => {
     const response = await axios.get(`/api/admin/quiz?id=${id}`);
@@ -70,8 +74,6 @@ const UserTable = ({ data }) => {
       toast.error(response.data.message);
     }
   });
-
-
 
   const handleContextMenu = (e, studentId) => {
     e.preventDefault();
@@ -84,26 +86,27 @@ const UserTable = ({ data }) => {
   };
 
   const handleShare = async (Quiz) => {
-    let date =  new Date(Quiz.updatedAt);
+    if(!Quiz.publish) return toast.error('Publish Before Share!');
+    let date = new Date(Quiz.updatedAt);
     const year = date.getFullYear();
-const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() is zero-based
-const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() is zero-based
+    const day = String(date.getDate()).padStart(2, "0");
 
-const formattedDate = `${day}-${month}-${year}`;
+    const formattedDate = `${day}-${month}-${year}`;
     // console.log(formattedDate);
     if (navigator.share) {
       try {
         await navigator.share({
-          title: 'Check out Test Link!',
-          text: `◉ Test No. ${data.length} \n◉ Total Question : ${Quiz.questions.length} \n◉ Date : ${formattedDate} \n Your MCQ Test link is Here.\n 👇👇👇👇👇 \n`,
-          url: linkWeb+Quiz._id, // Shares the current page URL
+          title: "Check out Test Link!",
+          text: `\n\n◉ Test No. ${data.length} \n◉ Total Question(s) : ${Quiz.questions.length} \n◉ Date : ${formattedDate} \n \n \n◉ Your MCQ Test link is Here.\n \n 👇👇👇👇👇 \n \n \n`,
+          url: linkWeb + Quiz._id, // Shares the current page URL
         });
-        toast.success('Link shared successfully');
+        toast.success("Link shared successfully");
       } catch (error) {
-        toast.error('Error sharing the link', error);
+        toast.error("Error sharing the link", error);
       }
     } else {
-      alert('Web Share API is not supported in your browser.');
+      alert("Web Share API is not supported in your browser.");
     }
   };
   const columns = useMemo(
@@ -115,9 +118,15 @@ const formattedDate = `${day}-${month}-${year}`;
       },
       {
         Header: "Icon",
-        accessor: (row)=> row,
+        accessor: (row) => row,
         Cell: ({ cell: { value } }) => (
-          <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIclu8YXkCr64EGkn23I4q_CeL1c5cD1WDug&s'} alt="avatar" className="w-12 h-12 object-cover rounded-full" />
+          <img
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIclu8YXkCr64EGkn23I4q_CeL1c5cD1WDug&s"
+            }
+            alt="avatar"
+            className="w-12 h-12 object-cover rounded-full"
+          />
         ),
         className: "max-sm:hidden sm:table-cell",
       },
@@ -130,35 +139,41 @@ const formattedDate = `${day}-${month}-${year}`;
         accessor: "createdAt",
         Cell: ({ cell: { value } }) => moment(value).fromNow(),
         sortType: (rowA, rowB) => {
-            const dateA = new Date(rowA.original.createdAt);
-            const dateB = new Date(rowB.original.createdAt);
-            return dateA - dateB;
-          }
+          const dateA = new Date(rowA.original.createdAt);
+          const dateB = new Date(rowB.original.createdAt);
+          return dateA - dateB;
+        },
       },
       {
         Header: "Courses",
         accessor: "forCourse",
         Cell: ({ cell: { value } }) => value.length,
         sortType: (rowA, rowB) => {
-          return rowA.original.forCourse.length - rowB.original.forCourse.length;
+          return (
+            rowA.original.forCourse.length - rowB.original.forCourse.length
+          );
         },
         className: "max-sm:hidden sm:table-cell",
       },
       {
         Header: "Questions",
         accessor: "questions",
-        Cell: ({ cell: { value } }) =>  + value.length,
+        Cell: ({ cell: { value } }) => +value.length,
         sortType: (rowA, rowB) => {
-          return rowA.original.questions.length - rowB.original.questions.length;
+          return (
+            rowA.original.questions.length - rowB.original.questions.length
+          );
         },
         className: "max-sm:hidden sm:table-cell",
       },
       {
         Header: "Done",
         accessor: "usersDone",
-        Cell: ({ cell: { value } }) =>  + value.length,
+        Cell: ({ cell: { value } }) => +value.length,
         sortType: (rowA, rowB) => {
-          return rowA.original.usersDone.length - rowB.original.usersDone.length;
+          return (
+            rowA.original.usersDone.length - rowB.original.usersDone.length
+          );
         },
       },
       {
@@ -167,7 +182,7 @@ const formattedDate = `${day}-${month}-${year}`;
         Cell: ({ cell: { value } }) =>
           value.publish ? (
             <span
-            onContextMenu={(e) => handleContextMenu(e, value._id)}
+              onContextMenu={(e) => handleContextMenu(e, value._id)}
               className="cursor-pointer"
             >
               Yes
@@ -180,16 +195,19 @@ const formattedDate = `${day}-${month}-${year}`;
               Publish
             </button>
           ),
-          sortType: (rowA, rowB) => {
-            return rowA.original.verified - rowB.original.verified;
-          },
+        sortType: (rowA, rowB) => {
+          return rowA.original.verified - rowB.original.verified;
+        },
       },
       {
         Header: "Edit",
         accessor: (row) => row,
         Cell: ({ cell: { value } }) => (
           <button
-            onClick={() => {setEditQuiz(value); setEditQuizDialog(true)}}
+            onClick={() => {
+              setEditQuiz(value);
+              setEditQuizDialog(true);
+            }}
             className="px-2 py-1 text-2xl bg-blue-500 text-white rounded-lg"
           >
             <FiEdit />
@@ -201,18 +219,25 @@ const formattedDate = `${day}-${month}-${year}`;
         accessor: (row) => row,
         Cell: ({ cell: { value } }) => (
           <div className="flex gap-4">
-          <button
-            onClick={() => {navigator.clipboard.writeText(`${linkWeb+value._id}`); toast.success("coppied successfully")}}
-            className="px-2 py-1 text-2xl bg-blue-500 text-white rounded-lg"
-          >
-            <FiCopy />
-          </button>
-          <button
-            onClick={() => {handleShare(value); }}
-            className="px-2 py-1 text-2xl bg-blue-500 text-white rounded-lg"
-          >
-            <FiShare2 />
-          </button></div>
+            <button
+              onClick={() => {
+                if(!value.publish) return toast.error('Publish Before Copy!');
+                navigator.clipboard.writeText(`${linkWeb + value._id}`);
+                toast.success("coppied successfully");
+              }}
+              className="px-2 py-1 text-2xl bg-blue-500 text-white rounded-lg"
+            >
+              <FiCopy />
+            </button>
+            <button
+              onClick={() => {
+                handleShare(value);
+              }}
+              className="px-2 py-1 text-2xl bg-blue-500 text-white rounded-lg"
+            >
+              <FiShare2 />
+            </button>
+          </div>
         ),
       },
     ],
@@ -238,12 +263,16 @@ const formattedDate = `${day}-${month}-${year}`;
     {
       columns,
       data: filteredData,
-      initialState: { pageIndex: 0, pageSize: 8, sortBy: [
-        {
-          id: 'createdAt',
-          desc: true,  // Set to true for descending order, false for ascending
-        },
-      ], },
+      initialState: {
+        pageIndex: 0,
+        pageSize: 8,
+        sortBy: [
+          {
+            id: "createdAt",
+            desc: true, // Set to true for descending order, false for ascending
+          },
+        ],
+      },
     },
     useSortBy,
     usePagination
@@ -253,8 +282,17 @@ const formattedDate = `${day}-${month}-${year}`;
     <>
       <div className="container mx-auto py-4 px-2">
         <div className="flex gap-2 items-center mb-4 w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+              clipRule="evenodd"
+            />
           </svg>
           <input
             type="text"
@@ -270,11 +308,11 @@ const formattedDate = `${day}-${month}-${year}`;
             className="table-auto w-full border-collapse border border-gray-400"
           >
             <thead className="bg-gray-700">
-              {headerGroups.map((headerGroup,i) => (
+              {headerGroups.map((headerGroup, i) => (
                 <tr key={i} {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column,j) => (
+                  {headerGroup.headers.map((column, j) => (
                     <th
-                    key={j}
+                      key={j}
                       {...column.getHeaderProps(column.getSortByToggleProps())}
                       className={`px-4 py-2 border border-gray-600 text-left cursor-pointer text-sm text-gray-50 ${
                         column.className || ""
@@ -311,16 +349,34 @@ const formattedDate = `${day}-${month}-${year}`;
               ))}
             </thead>
             <tbody {...getTableBodyProps()} className="">
-              {page.map((row,i) => {
+              {page.map((row, i) => {
                 prepareRow(row);
                 return (
-                  <tr key={i} {...row.getRowProps()} className="even:bg-gray-800/70 hover:bg-gray-800">
-                    {row.cells.map((cell,j) => (
-                      <td key={j}
+                  <tr
+                    key={i}
+                    {...row.getRowProps()}
+                    className="even:bg-gray-800/70 hover:bg-gray-800"
+                  >
+                    {row.cells.map((cell, j) => (
+                      <td
+                        key={j}
                         {...cell.getCellProps()}
-                        onClick={()=> cell.column.Header !== 'Name' && cell.column.Header !== 'Icon' && cell.column.Header !== '#' && cell.column.Header !== 'Created' || router.push(`quizes/${row.original._id}`)}
-                        className={`${cell.column.Header !== 'Name' && cell.column.Header !== 'Icon' && cell.column.Header !== '#' && cell.column.Header !== 'Created' || 'cursor-pointer text-blue-200'} border border-gray-600 px-4 py-2 text-sm ${cell.column.className || ""
-                          }`}
+                        onClick={() =>
+                          (cell.column.Header !== "Name" &&
+                            cell.column.Header !== "Icon" &&
+                            cell.column.Header !== "#" &&
+                            cell.column.Header !== "Created") ||
+                          router.push(`quizes/${row.original._id}`)
+                        }
+                        className={`${
+                          (cell.column.Header !== "Name" &&
+                            cell.column.Header !== "Icon" &&
+                            cell.column.Header !== "#" &&
+                            cell.column.Header !== "Created") ||
+                          "cursor-pointer text-blue-200"
+                        } border border-gray-600 px-4 py-2 text-sm ${
+                          cell.column.className || ""
+                        }`}
                       >
                         {cell.render("Cell")}
                       </td>
@@ -360,7 +416,9 @@ const formattedDate = `${day}-${month}-${year}`;
               type="number"
               defaultValue={pageIndex + 1}
               onChange={(e) => {
-                const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0;
+                const pageNumber = e.target.value
+                  ? Number(e.target.value) - 1
+                  : 0;
                 gotoPage(pageNumber);
               }}
               className="w-16 px-2 py-1 border border-gray-300 bg-gray-900/70 rounded-md"
@@ -371,7 +429,7 @@ const formattedDate = `${day}-${month}-${year}`;
             onChange={(e) => setPageSize(Number(e.target.value))}
             className="px-2 py-1 border border-gray-300 bg-gray-900 rounded-md"
           >
-            {[3,5,8, 10, 20, 30, 40, 50].map((size) => (
+            {[3, 5, 8, 10, 20, 30, 40, 50].map((size) => (
               <option key={size} value={size}>
                 Show {size}
               </option>
@@ -379,12 +437,12 @@ const formattedDate = `${day}-${month}-${year}`;
           </select>
         </div>
       </div>
-    
+
       {contextMenu.visible && (
         <div
           ref={contextMenuRef}
           className="absolute"
-          style={{ top: contextMenu.y+5, left: contextMenu.x+5 }}
+          style={{ top: contextMenu.y + 5, left: contextMenu.x + 5 }}
         >
           <button
             onClick={() => handlePublishQuiz(contextMenu.studentId)}
@@ -395,13 +453,18 @@ const formattedDate = `${day}-${month}-${year}`;
         </div>
       )}
 
-      {editQuiz && <UpdateQuiz confirmState={editQuizDialog} setConfirmState={(state) => {
-      setEditQuizDialog(state);
-      if (!state) setEditQuiz(null);
-    }} quizData={editQuiz} />}
+      {editQuiz && (
+        <UpdateQuiz
+          confirmState={editQuizDialog}
+          setConfirmState={(state) => {
+            setEditQuizDialog(state);
+            if (!state) setEditQuiz(null);
+          }}
+          quizData={editQuiz}
+        />
+      )}
     </>
   );
 };
 
 export default UserTable;
-
